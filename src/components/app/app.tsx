b9dfac36036c1816145566723c9397c30c3e91aa
Andrey Grachev <andreysgra@gmail.com>
2026-02-02 15:33:35 +0300

    Добавляет маршрутизацию в приложении.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,9 @@
 import MainPage from '../../pages/main-page/main-page';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
 
 
 type AppProps = {
@@ -6,7 +11,28 @@ type AppProps = {
 }
 
 function App({offersCount}: AppProps) {
-  return <MainPage offersCount={offersCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={`${AppRoute.Offer}/:id`}
+          element={<OfferPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
