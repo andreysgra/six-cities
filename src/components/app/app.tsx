1ad3c23e2d0c6235e097821bca42e32ec4f3aa8f
Andrey Grachev <andreysgra@gmail.com>
2026-02-02 15:24:21 +0300

    Создаёт компонент карточки предложения и подключает его к главной странице.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,12 @@
 import MainPage from '../../pages/main-page/main-page';
 
-function App() {
-  return <MainPage />;
+
+type AppProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppProps) {
+  return <MainPage offersCount={offersCount} />;
 }
 
 export default App;
