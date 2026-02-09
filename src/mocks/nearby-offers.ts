943e3701ecd0f32687ed686a4f7f5c0d4176d3c8
Andrey Grachev <andreysgra@gmail.com>
2026-02-09 22:14:59 +0300

    Добавляет тестовые данные для предложений неподалёку.

---
+++ src/mocks/nearby-offers.ts
@@ -0,0 +1,119 @@
+import {TOffers} from '../types/offer';
+
+export const nearbyOffers: TOffers = [
+  {
+    'id': '0b8ccfa1-7cf1-4268-b903-d6dd892dfdc7',
+    'title': 'Waterfront with extraordinary view',
+    'type': 'apartment',
+    'price': 222,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/8.jpg',
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 52.385540000000006,
+      'longitude': 4.902976,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 1.8
+  },
+  {
+    'id': 'e38fce2a-ba59-4ef5-a0d7-cc0a41c25956',
+    'title': 'Loft Studio in the Central Area',
+    'type': 'apartment',
+    'price': 302,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 52.397540000000006,
+      'longitude': 4.9099759999999995,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 4.8
+  },
+  {
+    'id': '70e9c700-8a7d-42e8-b49b-d189f696a0a0',
+    'title': 'Penthouse, 4-5 rooms + 5 balconies',
+    'type': 'room',
+    'price': 120,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/7.jpg',
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 52.37454,
+      'longitude': 4.881976,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 2.7
+  },
+  {
+    'id': '7925c508-d2f4-4375-bcd3-6681addc6ec0',
+    'title': 'Wood and stone place',
+    'type': 'hotel',
+    'price': 158,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/14.jpg',
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 52.367540000000005,
+      'longitude': 4.883976,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 1.3
+  },
+  {
+    'id': '13d4683d-92d3-4230-8957-009c4a7bedd3',
+    'title': 'Amazing and Extremely Central Flat',
+    'type': 'room',
+    'price': 173,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/12.jpg',
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.37454,
+        'longitude': 4.897976,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 52.361540000000005,
+      'longitude': 4.883976,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 2.8
+  }
+];
