d271638f008d68fc1863ceefe907c2b06d9216e7
Andrey Grachev <andreysgra@gmail.com>
2026-02-09 17:58:37 +0300

    Создаёт тестовые данные отзывов.

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,37 @@
+import {TReviews} from '../types/review';
+
+export const reviews: TReviews = [
+  {
+    'id': '2b9f1255-2b28-499a-b3fe-9ca222fd2cdd',
+    'comment': 'I stayed here for one night and it was an unpleasant experience.',
+    'date': '2026-01-16T21:00:00.143Z',
+    'rating': 2,
+    'user': {
+      'name': 'Sophie',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/4.jpg',
+      'isPro': true
+    }
+  },
+  {
+    'id': '0960793c-25e8-4fb2-b13e-3deff5b32597',
+    'comment': 'Beautiful space, fantastic location and atmosphere, really a wonderful place to spend a few days. Will be back.',
+    'date': '2026-01-16T21:00:00.143Z',
+    'rating': 5,
+    'user': {
+      'name': 'Jack',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/9.jpg',
+      'isPro': true
+    }
+  },
+  {
+    'id': '45da230a-ded2-46d7-91e7-219d17040515',
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2026-01-15T21:00:00.143Z',
+    'rating': 5,
+    'user': {
+      'name': 'Mollie',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/9.jpg',
+      'isPro': true
+    }
+  }
+];
