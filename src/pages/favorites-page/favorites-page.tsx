ac48075805518af956fc6afc086c1de04bab5509
Andrey Grachev <andreysgra@gmail.com>
2026-02-21 17:13:44 +0300

    Вносит изменения в реализацию логотипов.

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -1,6 +1,8 @@
 import {Fragment} from 'react';
 import Favorites from '../../components/favorites/favorites';
 import {TOffers} from '../../types/offer';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 type FavoritesPageProps = {
   offers: TOffers;
@@ -16,15 +18,9 @@ function FavoritesPage({offers}: FavoritesPageProps) {
       </main>
 
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img
-            className="footer__logo"
-            src="img/logo.svg"
-            alt="6 cities logo"
-            width={64}
-            height={33}
-          />
-        </a>
+        <Link className="footer__logo-link" to={AppRoute.Root}>
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width={64} height={33}/>
+        </Link>
       </footer>
     </Fragment>
   );
