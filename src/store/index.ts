a116a45cd021c3263ec1fc0ca9d268698deab3d8
Andrey Grachev <andreysgra@gmail.com>
2026-02-22 15:33:44 +0300

    Передаёт экземпляр Axios в хранилище.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,20 @@ import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from './reducer';
 import {setOffers} from './action';
 import {offers} from '../mocks/offers';
+import {createApi} from '../services/api/api';
 
-const store = configureStore({reducer});
+const api = createApi();
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
+
 
 store.dispatch(setOffers(offers));
 
