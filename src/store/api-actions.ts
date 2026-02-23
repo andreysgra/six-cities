2c87299b642ba361a9ab48833f44130a3fce3ad4
Andrey Grachev <andreysgra@gmail.com>
2026-02-23 11:08:46 +0300

    Добавляет новое действие для авторизации пользователя.

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -2,7 +2,8 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {TOffers} from '../types/offer';
 import {AxiosInstance} from 'axios';
 import {ApiRoute} from '../services/api/api-route';
-import {TUser} from '../types/user';
+import {TUser, TUserAuth} from '../types/user';
+import {saveToken} from '../services/token';
 
 export const fetchOffers = createAsyncThunk<TOffers, undefined, {extra: AxiosInstance}>(
   'offers/fetch',
@@ -19,5 +20,17 @@ export const fetchUserStatus = createAsyncThunk<TUser, undefined, {extra: AxiosI
     const {data} = await api.get<TUser>(ApiRoute.Login);
 
     return data;
+  }
+);
+
+export const loginUser = createAsyncThunk<TUserAuth['email'], TUserAuth, { extra: AxiosInstance }>(
+  'user/login',
+  async ({email, password}, {extra: api}) => {
+    const {data} = await api.post<TUser>(ApiRoute.Login, {email, password});
+    const {token} = data;
+
+    saveToken(token);
+
+    return email;
   }
 );
