7471d41628ad67485701037fd63df0eb518c3100
Andrey Grachev <andreysgra@gmail.com>
2026-02-24 15:25:47 +0300

    Создаёт действие и middleware для перенаправления на заданную страницу.

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,7 +1,10 @@
 import {createAction} from '@reduxjs/toolkit';
 import {TCityName} from '../types/city';
 import {TSortOption} from '../types/sorting';
+import {AppRoute} from '../const';
 
 export const setCity = createAction<TCityName>('city/set');
 
 export const setSorting = createAction<TSortOption>('sorting/set');
+
+export const redirectToRoute = createAction<AppRoute>('app/redirectToRoute');
